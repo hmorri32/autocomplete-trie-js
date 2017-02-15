18fc39a996b3ae7aa15d04203f277fa143744776
Hugh Morrison <hmorri32@gmail.com>
2017-02-15 16:48:22 -0700

    write pseudo code

--- scripts/trie.js
+++ scripts/trie.js
@@ -1,12 +1,17 @@
 import fs from "fs";
 
 class Node {
+  // the "vale" will reference the character in sequence
+  // create a children hash, keep reference to the parent,
+  // check to see if were at the end of our word
+
   constructor(value){
     this.value    = value;
     this.children = {};
     this.isWord   = false;
     this.parent   = null
   }
+  // Iterate through the parents, back to the root, to get words
   traverse() {
     var suggestions = [];
     var node        = this;
@@ -25,18 +30,26 @@ class Trie {
     this.count = 0;
   }
   insert(word) {
+// if we pass through nonsense , throw a firiggin error
     if(word === undefined || word === null){
       throw new Error('error');
     }
 
     let node = this.head;
+// for every character in the word passed through
+// check to see if the letter exists in our children
+// if it doesnt, create a new node with that letter
+// assign parent to the node
+
     for(let i = 0; i < word.length; i++){
       if(!node.children[word[i]]){
         node.children[word[i]] = new Node(word[i])
         node.children[word[i]].parent = node;
         // console.log(node.children)
       }
+// proceed to the next level down
       node = node.children[word[i]];
+// if were at the end of the line, declare it as a word, up our counter
       if(i === word.length-1){
         node.isWord = true;
         this.count ++;
@@ -44,10 +57,16 @@ class Trie {
       }
     }
   }
+
+// returns every word weve inserted given the first letter(s)
+
   suggest(letters) {
     let node        = this.head;
     let suggestions = [];
-    
+
+// Loop through every letter that we pass through
+// if the node contains the letters, if it doesnt, return array.
+
     for(let i = 0; i < letters.length; i++){
       if(node.children[letters[i]]){
         node = node.children[letters[i]];
@@ -55,10 +74,17 @@ class Trie {
         return suggestions
       }
     }
+// recursion, yo!
+
     findWords(node, suggestions)
     return suggestions
   }
 }
+
+
+// Recursive function used to find all words in a node
+// if node is already at a word, push it.
+// if it isnt, keep iterating through each child.
 
 function findWords(node, array) {
   if (node.isWord === true) {
